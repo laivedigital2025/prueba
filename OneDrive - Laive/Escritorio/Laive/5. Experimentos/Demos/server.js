126132b462f18f0f92c1e2214d707090fb626d02
Laive Digital <camila.cubas@laive.pe>
2025-04-28 21:29:14 -0500

    Arreglar path de config/db para producción

--- OneDrive - Laive/Escritorio/Laive/5. Experimentos/Demos/server.js
+++ OneDrive - Laive/Escritorio/Laive/5. Experimentos/Demos/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { getConnection } = require('../config/db'); 
+const { getConnection } = require('./config/db');
 
 const app = express();
 const port = process.env.PORT || 3000;
